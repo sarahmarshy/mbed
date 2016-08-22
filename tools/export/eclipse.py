1bcdc2cbc92f613eb07424723a6b261aba37446c
Sarah Marsh <Sarah.Marsh@arm.com>
2016-08-22 11:08:50 -0500

    Initial eclipse support

---
+++ tools/export/eclipse.py
@@ -0,0 +1,45 @@
+from os.path import join, dirname, exists, realpath, relpath, basename
+from os import makedirs
+
+from tools.export.gccarm import GccArm
+from distutils.spawn import find_executable
+
+class Eclipse(GccArm):
+    def generate(self):
+        ctx = {
+            'name': self.project_name,
+            'elf_location': join('.build',self.target,
+                                 'GCC_ARM',self.project_name)+'.elf',
+            'c_symbols': self.toolchain.get_symbols(),
+            'asm_symbols': self.toolchain.get_symbols(True),
+            'target': self.target
+        }
+        for f,bp in self.resources.file_basepath.items():
+            self.resources.file_basepath[realpath(f)] = realpath(bp)
+        ctx['include_paths'] = []
+        for inc in self.resources.inc_dirs:
+            i = realpath(join(self.export_dir,inc))
+            bp = self.resources.file_basepath[realpath(i)]
+            res_path = join(basename(bp), relpath(i, bp))
+            ctx['include_paths'].append(res_path)
+
+        if not exists(join(self.export_dir,'eclipse-extras')):
+            makedirs(join(self.export_dir,'eclipse-extras'))
+
+
+        self.gen_file('cdt/py_ocd_settings.tmpl', ctx,
+                      join('eclipse-extras',self.target+'_py_ocd_settings.launch'))
+        self.gen_file('cdt/necessary_software.tmpl', ctx,
+                      join('eclipse-extras','necessary_software.p2f'))
+
+        cproj = relpath('.cproject',self.export_dir)
+        self.gen_file('cdt/.cproject.tmpl', ctx,
+                      cproj)
+        proj = relpath('.project',self.export_dir)
+        self.gen_file('cdt/.project.tmpl', ctx,
+                      proj)
+
+
+
+
+
